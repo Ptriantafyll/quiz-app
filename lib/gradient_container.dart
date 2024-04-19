bfdf978e53a9a256e10922f6cc1c450219013f2d
Ptriantafyll <ptriantafyll@gmail.com>
2024-04-19 21:01:51 +0300

    Added page on state of widget

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,26 +1,42 @@
+import 'package:flutter/widgets.dart';
+import 'package:quiz_app/questions_page.dart';
 import 'package:quiz_app/start_page.dart';
-// import 'package:quiz_app/styled_text.dart';
 import 'package:flutter/material.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
-class GradientContainer extends StatelessWidget {
+class GradientContainer extends StatefulWidget {
   const GradientContainer({super.key, required this.gradientColors});
 
   final List<Color> gradientColors;
+
+  @override
+  State<StatefulWidget> createState() {
+    return _GradientContainerState();
+  }
+}
+
+class _GradientContainerState extends State<GradientContainer> {
+  Widget activePage = const StartPage();
+
+  void switchPage() {
+    setState(() {
+      activePage = const QuestionsPage();
+    });
+  } 
 
   @override
   Widget build(context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: gradientColors,
+          colors: widget.gradientColors,
           begin: startAlignment,
           end: endAlignment,
         ),
       ),
-      child: const Center(child: StartPage()),
+      child: Center(child: activePage),
     );
   }
 }
