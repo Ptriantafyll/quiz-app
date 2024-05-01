77ee6e1a6881ff917b01a7dafc592c49e3171434
Ptriantafyll <ptriantafyll@gmail.com>
2024-05-01 23:16:49 +0300

    Added shuffled answers

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -3,4 +3,10 @@ class QuizQuestion {
 
   final String text;
   final List<String> answers;
+
+  List<String> getShuffledAnswers() {
+    var shuffledList = List.of(answers);
+    shuffledList.shuffle();
+    return shuffledList;
+  }
 }
