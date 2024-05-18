af3763f2a85fde5b8909b85fa6e9ebda27a661a6
Ptriantafyll <ptriantafyll@gmail.com>
2024-05-18 18:38:05 +0300

    Added results page and functionality to restart quiz

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class QuestionsSymmary extends StatelessWidget {
   const QuestionsSymmary(this.summaryData, {super.key});
@@ -8,26 +7,60 @@ class QuestionsSymmary extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return Column(
-      children: summaryData.map((data) {
-        return Row(
-          children: [
-            Text(((data['question_index'] as int) + 1).toString()),
-            Expanded(
-              child: Column(
+    return SizedBox(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: summaryData.map((data) {
+            return IntrinsicHeight(
+              child: Row(
                 children: [
-                  Text(data['question'] as String),
-                  const SizedBox(
-                    height: 5,
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: CircleAvatar(
+                      radius: 15,
+                      // backgroundColor: Colors.red,
+                      backgroundColor:
+                          data['correct_answer'] == data['user_answer']
+                              ? const Color.fromARGB(255, 0, 255, 34)
+                              : Colors.red,
+                      child: Text(
+                          ((data['question_index'] as int) + 1).toString()),
+                    ),
                   ),
-                  Text(data['user_answer'] as String),
-                  Text(data['correct_answer'] as String),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          data['question'] as String,
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          data['user_answer'] as String,
+                          style: const TextStyle(
+                              color: Color.fromARGB(255, 195, 255, 0)),
+                        ),
+                        Text(
+                          data['correct_answer'] as String,
+                          style: const TextStyle(
+                              color: Color.fromARGB(255, 0, 255, 34)),
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
-            ),
-          ],
-        );
-      }).toList(),
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
